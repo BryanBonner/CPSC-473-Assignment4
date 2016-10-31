c6e29404cd13b586af1e5adaddfebcf60b094254
BryanBonner <bonnerbryan@gmail.com>
2016-10-31 14:55:11 -0700

    spacing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-	  router = express.Router();
+    router = express.Router();
 
 router.get('/', function(req, res) {
-		console.log('get request');
-		res.render('index');
+	console.log('get request');
+	res.render('index');
 });
 
 module.exports = router;
